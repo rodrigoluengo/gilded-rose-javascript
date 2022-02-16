f88aac13aa4fc1ef06f84493ce7477e08c859af3
Rodrigo Luengo <rpl@outlook.com>
2022-02-16 09:59:17 -0300

    should drop to 0 in quality when sell_in is 0 okay

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -95,7 +95,7 @@ describe("Gilded Rose", function () {
     it("should drop to 0 in quality when sell_in is 0", function () {
       const items = [new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20)]
       const [item] = items;
-
+      update_quality(items);
       expect(item.quality).toBe(0);
     });
   });
