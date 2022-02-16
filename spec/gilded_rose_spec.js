79472ccb450a53faafc9f4c55d3416d2033448aa
Rodrigo Luengo <rpl@outlook.com>
2022-02-16 09:10:38 -0300

    should decrease 1 in sell_in and quality properties

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,17 @@
-describe("Gilded Rose", function() {
+const { Item, update_quality } = require('../src/gilded_rose');
 
-  it("should do something", function() {
-    update_quality();
+describe("Gilded Rose", function () {
+
+  describe("Test default items", function () {
+
+    it("shoud decrease 1 in sell_in and quality properties", function () {
+      const items = [new Item('+5 Dexterity Vest', 10, 20)]
+      const [dexterity] = items;
+
+      expect(dexterity.sell_in).toBe(9);
+      expect(dexterity.quality).toBe(19);
+    });
+
   });
 
 });
