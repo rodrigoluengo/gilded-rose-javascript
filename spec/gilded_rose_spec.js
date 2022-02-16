7e2a553c56caecff30bd3b2fc543f50fae910183
Rodrigo Luengo <rpl@outlook.com>
2022-02-16 09:13:53 -0300

    should quality degrades twice

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -12,6 +12,14 @@ describe("Gilded Rose", function () {
       expect(dexterity.quality).toBe(19);
     });
 
+    it("should quality degrades twice", function () {
+      const items = [new Item('+5 Dexterity Vest', 0, 10)]
+      const [dexterity] = items;
+
+      expect(dexterity.sell_in).toBe(-1);
+      expect(dexterity.quality).toBe(8);
+    })
+
   });
 
 });
