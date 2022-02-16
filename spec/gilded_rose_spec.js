c42c8eb0973f2477fdd71846c615d2f484e04aa3
Rodrigo Luengo <rpl@outlook.com>
2022-02-16 09:49:56 -0300

    should increase by 2 in quality when sell_in is 10 okay

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -67,7 +67,7 @@ describe("Gilded Rose", function () {
     it("should increase by 2 in quality when sell_in is 10", function () {
       const items = [new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20)]
       const [item] = items;
-
+      update_quality(items);
       expect(item.quality).toBe(22);
     });
   });
